0befb4c6751c1b0fe5035226714ec8d10afe5c91
Mitchell van Manen <ma.van.manen@live.nl>
2017-07-30 16:00:17 +0200

    Added pathhelper tests

---
+++ Pugzor.Test/PathHelperTests.cs
@@ -0,0 +1,43 @@
+﻿using Pugzor.Core.Helpers;
+using Xunit;
+
+namespace Pugzor.Test
+{
+    [Trait("Category", "Helper")]
+    public class PathHelperTests
+    {
+        private const string RELATIVE_PATH = "this/is/relative.pug";
+        private const string ABSOLUTE_PATH_1 = "/this/is/absolue.pug";
+        private const string ABSOLUTE_PATH_2 = "~/this/is/absolue.pug";
+
+
+        [Fact]
+        public void PathHelperIsRelativePath_WithRelativePath_ReturnsTrue()
+        {
+            Assert.True(PathHelper.IsRelativePath(RELATIVE_PATH));
+        }
+
+        [Theory]
+        [InlineData(ABSOLUTE_PATH_1)]
+        [InlineData(ABSOLUTE_PATH_2)]
+        public void PathHelperIsRelativePath_WithNonRelativePath_ReturnsFalse(string path)
+        {
+            Assert.False(PathHelper.IsRelativePath(path));
+            Assert.False(PathHelper.IsRelativePath(path));
+        }
+
+        [Theory]
+        [InlineData(ABSOLUTE_PATH_1)]
+        [InlineData(ABSOLUTE_PATH_2)]
+        public void PathHelperIsAbsolutePath_WithAbsolutePath_ReturnsTrue(string path)
+        {
+            Assert.True(PathHelper.IsAbsolutePath(path));
+        }
+
+        [Fact]
+        public void PathHelperIsAbsolutePath_WithNonAbsolutePath_ReturnsTrue()
+        {
+            Assert.False(PathHelper.IsAbsolutePath(RELATIVE_PATH));
+        }
+    }
+}
