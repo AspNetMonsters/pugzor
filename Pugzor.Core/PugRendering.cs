4d5c186fba7368eec21e02390173cf7141f90c6d
Serhii Kuzmychov <ku3mich@gmail.com>
2017-09-13 10:01:12 +0300

    added basedir, pretty to pugzor config options, passed them to pug

--- Pugzor.Core/PugRendering.cs
+++ Pugzor.Core/PugRendering.cs
@@ -5,21 +5,29 @@ using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Pugzor.Core.Abstractions;
 using Pugzor.Core.Helpers;
+using Microsoft.Extensions.Options;
 
 namespace Pugzor.Core
 {
     public class PugRendering : IPugRendering
     {
         private readonly INodeServices _nodeServices;
+        private PugzorViewEngineOptions Options;
 
-        public PugRendering(INodeServices nodeServices)
+        public PugRendering(INodeServices nodeServices, IOptions<PugzorViewEngineOptions> options)
         {
             _nodeServices = nodeServices;
             var tempDirectory = TemporaryDirectoryHelper.CreateTemporaryDirectory();
             EmbeddedFileHelper.ExpandEmbeddedFiles(tempDirectory);
+            Options = options.Value;
         }
 
         public async Task<string> Render(FileInfo pugFile, object model, ViewDataDictionary viewData, ModelStateDictionary modelState) =>
-            await _nodeServices.InvokeAsync<string>("pugcompile", pugFile.FullName, model, viewData, modelState).ConfigureAwait(false);
+            await _nodeServices.InvokeAsync<string>("pugcompile", pugFile.FullName, model, viewData, modelState, new
+            {
+                pretty = Options.Pretty ? "\t" : null,
+                basedir = Options.BaseDir
+            })
+            .ConfigureAwait(false);
     }
 }
