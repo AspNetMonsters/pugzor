4a46b5f6241ed70b43ad0aa1037e8a80b00eea5d
Serhii Kuzmychov <ku3mich@gmail.com>
2017-09-13 10:01:12 +0300

    added basedir, pretty to pugzor config options, passed them to pug

--- Pugzor.Core/PugzorViewEngineOptions.cs
+++ Pugzor.Core/PugzorViewEngineOptions.cs
@@ -5,5 +5,7 @@ namespace Pugzor.Core
     public class PugzorViewEngineOptions
     {
         public IList<string> ViewLocationFormats { get; } = new List<string>();
+        public string BaseDir { get; set; }
+        public bool Pretty { get; set; }
     }
 }
